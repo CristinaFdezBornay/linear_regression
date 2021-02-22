5be5381ccc5eef5cfacb8b830d10644aace721dc
Cristina Fernandez Bornay <cristinafernandezbornay@outlook.com>
2021-02-22 17:20:44 +0100

    feat(train): not working train.py

--- train.py
+++ train.py
@@ -2,13 +2,15 @@
 try:
     import argparse
     import pandas as pd
+    import numpy as np
     import pickle
+    import matplotlib.pyplot as plt
 except:
     print('[Import error] Please run <pip3 install -r requirements.txt>')
     exit()
 
 def parse_arg():
-    parser = argparse.ArgumentParser(prog='calculate_price', usage='%(prog)s [-h] datafile.csv', description='Program to train a model to calculate the price of a car for a given milage.')
+    parser = argparse.ArgumentParser(prog='train', usage='%(prog)s [-h] datafile.csv', description='Program to train a model to calculate the price of a car for a given milage.')
     parser.add_argument('datafile', help='the .csv file containing the data to train the model')
     args = parser.parse_args()
     return args
@@ -16,62 +18,65 @@ def parse_arg():
 def read_csv(datafile):
     try:
         f = pd.read_csv(datafile)
-        km = f.get('km')
-        price = f.get('price')
-        if len(km) == 0:
-            km = []
-        if len(price) == 0:
-            price = []
-        if len(km) != len(price):
-            return [], []
+        km = np.array(f.get('km'))
+        price = np.array(f.get('price'))
         return km, price
     except:
         raise NameError('\n[Read error]\nWrong file format. Make sure you give an existing .csv file as argument.\n')
 
-def calculate_price(mileage, tetha0, tetha1):
+def normalize(X):
     try:
-        estimated_price = tetha0 + (tetha1 * mileage)
-        return estimated_price
+        Xnorm = (X - X.mean()) / X.std()
+        return Xnorm
     except:
         raise NameError('\n[Process error]\nThere has been an error while processing the information.\n')
 
-def calculate_loss(km, price, tetha0, tetha1):
-    try:
-        loss = 0
-        for i in range(len(km)):
-            predicted_price_i = calculate_price(km[i], tetha0, tetha1)
-            diff_price = price[i] - predicted_price_i
-            loss += diff_price ** 2
+def train_model(KM, PRICE):
+
+        N = KM.shape[0]
+        tetha0 = 0
+        tetha1 = 0
+        learning_rate = 0.1
+        number_iterations = 100
+
+        for i in range(number_iterations):
+            estimated_price = tetha0 + (KM * tetha1)
+            print(estimated_price)
+            print(PRICE)
+            loss = estimated_price - PRICE
+            print(loss)
+            print('loss      : {}'.format(np.sum(loss)))
+            print('loss * KM : {}'.format(np.sum(loss * KM)))
+            tetha0_gradient = (2/N) * np.sum(loss)
+            tetha1_gradient = (2/N) * np.sum(loss * KM)
+            print('tetha0_g : {}'.format(tetha0_gradient))
+            print('tetha1_g : {}'.format(tetha1_gradient))
+            tetha0 = tetha0 - (learning_rate * tetha0_gradient)
+            tetha1 = tetha1 - (learning_rate * tetha1_gradient)
+            print('tetha0 : {}'.format(tetha0))
+            print('tetha1 : {}'.format(tetha1))
+            print('tetha0: {:.5f} || tetha1: {:.5f}'.format(tetha0, tetha1))        
             i += 1
-        loss /= len(km)
-        return loss
+
+        tethas = { 'tetha0': str(tetha0), 'tetha1': str(tetha1) }
+        return tethas
+
+def save_tethas(tethas):
+    try:
+        outfile = open('tethas', 'wb')
+        pickle.dump(tethas, outfile)
+        outfile.close()
     except:
         raise NameError('\n[Process error]\nThere has been an error while processing the information.\n')
 
 def main():
     try:
-        # args = parse_arg()
-        # km, price = read_csv(args.datafile)
-        # tetha0 = 0
-        # tetha1 = 0
-        # mean_km = sum(km) / len(km)
-        # mean_price = sum(price) / len(price)
-
-        # # accuracy = float('inf')
-        # # while accuracy > 1:
-
-        # #     tmp_tetha0 =
-        # # print(km)
-        # # print(price)
-        # print(calculate_loss(km, price, tetha0, tetha1))
-        # mileage = get_mileage()
-        # price = calculate_price(mileage, tetha0, tetha1)
-        # display_price(price, mileage, tetha0, tetha1)
-
-        tethas_dict = { 'tetha0': '345.2', 'tetha1': '24.5' }
-        outfile = open('tethas', 'wb')
-        pickle.dump(tethas_dict, outfile)
-        outfile.close()
+        args = parse_arg()
+        km, price = read_csv(args.datafile)
+        KM = normalize(km)
+        PRICE = normalize(price)
+        tethas = train_model(KM, PRICE)
+        save_tethas(tethas)
     except NameError as e:
         print(e)
 
